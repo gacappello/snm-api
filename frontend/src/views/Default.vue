1f3677fbbeb3200d2f966c660138facf474afa0d
Giovanni Alberici <albericigiovanni03@gmail.com>
2024-06-22 22:30:39 +0200

    Default view

--- frontend/src/views/Default.vue
+++ frontend/src/views/Default.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>Non abbiamo trovato cio che cerchi</div>
+  <v-container fluid class="mt-12 text-grey-lighten-2">
+    <v-row dense>
+      <v-col cols="12" class="text-center text-h1">
+        <h1>4<span class="text-ternary">0</span>4</h1>
+      </v-col>
+      <v-col cols="12" class="text-center text-h3">
+        <h2><span class="text-ternary">Not</span> Found</h2>
+      </v-col>
+    </v-row>
+    <v-row dense class="mt-12 pa-12">
+      <v-col cols="12" class="text-center text-h5">
+        <h2>We haven't found what you want!</h2>
+      </v-col>
+      <v-col cols="12" class="text-center text-h6">
+        <h3>where did you want to go?</h3>
+      </v-col>
+    </v-row>
+    <v-row dense class="mt-12 pa-12">
+      <v-col cols="12" class="text-center text-button">
+        <a class="text-ternary text-decoration-none" href="/home">
+          Try here
+          <v-icon size="small">fa-solid fa-arrow-right</v-icon>
+        </a>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script></script>
